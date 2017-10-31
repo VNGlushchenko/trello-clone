0b1d43bf41d76d0ac5846ea02ffc143812eefdff
Vladimir Glushchenko <v.glushchenko@gmail.com>
2017-10-31 20:11:26 +0200

    server.js was changed

--- src/app/server/server.js
+++ src/app/server/server.js
@@ -5,3 +5,13 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
 var config = require('./config');
+//var User   = require('./app/models/user'); // get our mongoose model
+var port = process.env.PORT || 8000;
+mongoose.connect(config.database);
+app.set('jwtSecret', config.secret);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+//routes
+app.listen(port); //ng serve --proxy-config proxy-conf.json
+console.log('Server is run at http://localhost:' + port);
