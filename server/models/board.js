31946f2e1db4d6fb8c980d22a51a3574f3686f17
Vladimir Glushchenko <v.glushchenko@gmail.com>
2017-11-06 11:31:11 +0200

    boardSchema was changed

--- server/models/board.js
+++ server/models/board.js
@@ -3,12 +3,7 @@ var Schema = mongoose.Schema;
 
 var boardSchema = new Schema(
   {
-    title: { type: String, required: true },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    }
+    title: { type: String, required: true }
   },
   {
     timestamps: true
